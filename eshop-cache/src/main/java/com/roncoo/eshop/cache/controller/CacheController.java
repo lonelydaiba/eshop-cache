85315f9836f5db42a079020058ce1a8abadefbe3
lonelydaiba <dai_ba@163.com>
2019-09-23 19:46:38 +0800

    [add]缓存预热的代码提交

--- eshop-cache/src/main/java/com/roncoo/eshop/cache/controller/CacheController.java
+++ eshop-cache/src/main/java/com/roncoo/eshop/cache/controller/CacheController.java
@@ -2,14 +2,15 @@ package com.roncoo.eshop.cache.controller;
 
 import javax.annotation.Resource;
 
-import com.alibaba.fastjson.JSONObject;
-import com.roncoo.eshop.cache.rebuild.RebuildCacheQueue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
 import com.roncoo.eshop.cache.model.ProductInfo;
 import com.roncoo.eshop.cache.model.ShopInfo;
+import com.roncoo.eshop.cache.prewarm.CachePrewarmThread;
+import com.roncoo.eshop.cache.rebuild.RebuildCacheQueue;
 import com.roncoo.eshop.cache.service.CacheService;
 
 /**
@@ -42,16 +43,20 @@ public class CacheController {
 		ProductInfo productInfo = null;
 		
 		productInfo = cacheService.getProductInfoFromReidsCache(productId);
-		System.out.println("=================从redis中获取缓存，商品信息=" + productInfo);   
+		if(productInfo != null) {
+			System.out.println("=================从redis中获取缓存，商品信息=" + productInfo);   
+		}
 		
 		if(productInfo == null) {
 			productInfo = cacheService.getProductInfoFromLocalCache(productId);
-			System.out.println("=================从ehcache中获取缓存，商品信息=" + productInfo);   
+			if(productInfo != null) {
+				System.out.println("=================从ehcache中获取缓存，商品信息=" + productInfo); 
+			}
 		}
 		
 		if(productInfo == null) {
 			// 就需要从数据源重新拉去数据，重建缓存，但是这里先不讲
-			String productInfoJSON = "{\"id\": 3, \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\"}";
+			String productInfoJSON = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\"}";
 			productInfo = JSONObject.parseObject(productInfoJSON, ProductInfo.class);
 			// 将数据推送到一个内存队列中
 			RebuildCacheQueue rebuildCacheQueue = RebuildCacheQueue.getInstance();
@@ -79,6 +84,12 @@ public class CacheController {
 		}
 		
 		return shopInfo;
+	}
+	
+	@RequestMapping("/prewarmCache")
+	@ResponseBody
+	public void prewarmCache() {
+		new CachePrewarmThread().start();
 	}
 	
 }
